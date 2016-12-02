2b158a67e1c0dad9f89cf075bf31cab7f4e83d52
amaendeepm <amandeepm@gmx.com>
2016-12-02 20:06:23 +0100

    Update campaignmonitor.go

--- campaignmonitor/campaignmonitor.go
+++ campaignmonitor/campaignmonitor.go
@@ -13,8 +13,72 @@ import (
 
 
 // Two parameters that need defining, or need to come from global context
-// 1. CM_SignupListID is Subscriber List ID
+// 1. CM_SignupListID : Subscriber List ID
 // 2. CM_AuthHeader : Actual Authoriation Header for your Campaign Monitor Account
+
+
+//PUBLIC: 1
+//Refer: https://www.campaignmonitor.com/api/subscribers/#adding_a_subscriber
+
+func addToSignUpList(jsonStr []byte, r *http.Request) (string, error) {
+	return add2CMList(buildEndpointForSubscriberAdding(CM_SignupListID), jsonStr, r)
+}
+
+//PUBLIC: 2
+//Refer: https://www.campaignmonitor.com/api/subscribers/#updating_a_subscriber
+
+func updateSubscriber(jsonStr []byte, r *http.Request, email string) (string, error) {
+
+	putEp:= buildEndpointForSubscriberUpdating(CM_SignupListID, email)
+
+	ctx := appengine.NewContext(r)
+	client := &http.Client{
+		Transport: &urlfetch.Transport{Context: ctx},
+	}
+
+	req, err := http.NewRequest("PUT", putEp, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Authorization", "Basic " + CM_AuthHeader)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Errorf(ctx, "error-1: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Errorf(ctx, "error-2: %v", err)
+		return "",err
+	}
+
+	log.Debugf(ctx,"Response Received: %d",string(body))
+
+	js, err := simplejson.NewJson(body)
+
+	if err != nil {
+		log.Errorf(ctx,"error-3: %v", err)
+		return "",err
+	}
+
+	respStr,_ := js.String()
+
+	log.Debugf(ctx, "Response Body: %s", string(respStr))
+
+	return respStr, nil
+}
+
+//PUBLIC : 3 : Sending Transaction Email
+//Refer: https://www.campaignmonitor.com/api/transactional/
+
+func sendCMTxnEmail(templateID string, jsonStr []byte, r *http.Request) (string, error) {
+
+	return invokeCM_POST(buildEndpointForTxnEmail(templateID), jsonStr, r)
+}
+
+
+//INTERNAL USE METHODS
 
 func buildEndpointForTxnEmail(templateID string) string {
 	// Following this Spec: https://www.campaignmonitor.com/api/transactional/#send_a_smart_email
@@ -26,20 +90,17 @@ func buildEndpointForSubscriberAdding(listID string) string { //
 	return "https://api.createsend.com/api/v3.1/subscribers/"+listID+".json"
 }
 
-func addToSignUpList(jsonStr []byte, r *http.Request) (string, error) {
-	return add2CMList(buildEndpointForSubscriberAdding(CM_SignupListID), jsonStr, r)
-
+func buildEndpointForSubscriberUpdating(listID string, email string) string { //
+	// Following this Spec: https://www.campaignmonitor.com/api/subscribers/
+	return "https://api.createsend.com/api/v3.1/subscribers/"+listID+".json?email="+email
 }
+
 
 func add2CMList(endpoint string, jsonStr []byte, r *http.Request) (string, error) {
 
 	return invokeCM_POST(endpoint, jsonStr, r)
 }
 
-func sendCMTxnEmail(templateID string, jsonStr []byte, r *http.Request) (string, error) {
-
-	return invokeCM_POST(buildEndpointForTxnEmail(templateID), jsonStr, r)
-}
 
 func invokeCM_POST(endpoint string, payload []byte, r *http.Request) (string, error) {
 
